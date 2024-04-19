d16a02358127105855b30f69807addb7cff49722
georgievcodes <atjoro88@gmail.com>
2024-04-19 21:02:32 +0300

    Add project files.

---
+++ Ticket_Richard/Controllers/GridController.cs
@@ -0,0 +1,29 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ticket_Richard.Data;
+using Ticket_Richard.Models;
+
+namespace Ticket_Richard.Controllers
+{
+    public class GridController : Controller
+    {
+        public ActionResult Orders_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = new OrdersCollection().Get_OrdersSet();
+
+            var dsResult = result.ToDataSourceResult(request);
+            return Json(dsResult);
+        }
+
+        public ActionResult DropDownListData([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = new OrdersCollection().Get_OrdersSet().Select(x => x.ShipCity).Distinct();
+            return Json(result.ToList());
+        }
+    }
+}
---
+++ Ticket_Richard/Data/OrdersCollection.cs
@@ -0,0 +1,32 @@
+﻿using Ticket_Richard.Models;
+
+namespace Ticket_Richard.Data
+{
+    public class OrdersCollection
+    {
+        private readonly IEnumerable<OrderViewModel> orders = new List<OrderViewModel>
+        {
+            new OrderViewModel { OrderID = 1, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Berlin", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 2, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "London", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 3, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Amsterdam", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 4, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Berlin", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 5, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Berlin", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 6, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Amsterdam", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 7, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Amsterdam", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 8, Freight = 1, OrderDate = new DateTime(2016, 9, 15), ShipCity = "London", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 9, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Berlin", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 10, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "London", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 11, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Amsterdam", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 12, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Amsterdam", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 13, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Berlin", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 14, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Amsterdam", ShipName = "TransitCo" },
+            new OrderViewModel { OrderID = 15, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "London", ShipName = "FraightCo" },
+            new OrderViewModel { OrderID = 16, Freight = 12, OrderDate = new DateTime(2016, 9, 15), ShipCity = "Berlin", ShipName = "TransitCo" },
+        };
+
+        public IEnumerable<OrderViewModel> Get_OrdersSet()
+        {
+            return orders;
+        }
+    }
+}
